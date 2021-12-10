1b1b53434d56a21ef7505cede34e09f38fdaa0be
DaShaun <dashaun@dashaun.com>
2021-12-09 21:06:11 -0600

    add variables to cloudflare workspace

--- tfe_variable.tf
+++ tfe_variable.tf
@@ -5,3 +5,11 @@ resource "tfe_variable" "cloudflare_email" {
   workspace_id = tfe_workspace.workspace.id
   sensitive    = true
 }
+
+resource "tfe_variable" "cloudflare_api_token" {
+  key          = "cloudflare_api_token"
+  value        = var.cloudflare_api_token
+  category     = "terraform"
+  workspace_id = tfe_workspace.workspace.id
+  sensitive    = true
+}
